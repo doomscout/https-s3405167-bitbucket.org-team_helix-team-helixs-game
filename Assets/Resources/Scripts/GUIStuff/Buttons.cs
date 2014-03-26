203eaacf2ad2b8ebe09f73fdfd09c7054a95852f
Isaac Chui <greycreeze2345@yahoo.com.au>
2014-03-26 16:15:03 +1100

    Character Creator done Will work on other guis later

---
+++ Assets/Resources/Scripts/GUIStuff/Buttons.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+	int Hp = 50;
+	int Mp = 50;
+	string Title = "Character Creator";
+	int size;
+	void Start(){
+		 size = Title.Length;
+		Debug.Log(size);
+	}
+	void OnGUI () {
+
+		// Make a background box
+		GUI.Box(new Rect(10,10,size * 10,200), "Character Creator" +
+			"\nHealth" + "\n\n\nMana");
+		//Displays Hit Points
+		GUI.Box(new Rect(30,45,(size * 10 - 40),20), Hp.ToString());
+		// Click button to decrease Health
+		if(GUI.Button(new Rect(10,45,20,20), "<-")) {
+			if (Hp != 10)
+			{
+			Hp -= 10;
+			}
+	    }
+		// Button to increase Health
+		if(GUI.Button(new Rect((size * 10 - 10),45,20,20), "->")) {
+			Hp += 10;
+		}
+		GUI.Box(new Rect(30,90,(size * 10 - 40),20), Mp.ToString());
+		// Click button to decrease Health
+
+		if(GUI.Button(new Rect(10,90,20,20), "<-")) {
+			if (Mp != 10)
+			{
+				Mp -= 10;
+			}
+		}
+		// Button to increase Health
+		if(GUI.Button(new Rect((size * 10 - 10),90,20,20), "->")) {
+			Mp += 10;
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/GUIStuff/InputTest.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTest : MonoBehaviour {
+
+	void Update() {
+		foreach (char c in Input.inputString) {
+			if (c == "\b"[0])
+				if (guiText.text.Length != 0)
+					guiText.text = guiText.text.Substring(0, guiText.text.Length - 1);
+			
+			else
+				if (c == "\n"[0] || c == "\r"[0])
+					print("User entered his name: " + guiText.text);
+			else
+				guiText.text += c;
+		}
+	}
+}
