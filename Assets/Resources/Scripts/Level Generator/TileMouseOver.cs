04cd78f0dee49e717e13f117e22dff929c7714ba
Ke Yi Ren <keyiren@hotmail.com>
2014-04-02 12:04:05 +1100

    Update with my own level and mouse over on Tile with change color

---
+++ Assets/Resources/Scripts/Level Generator/TileMouseOver.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileMouseOver : MonoBehaviour {
+	public Color highlightColor;
+	Color normalColor;
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = normalColor;
+	}
+
+	void Update(){
+		Ray ray = Camera.mainCamera.ScreenPointToRay(Input.mousePosition);
+	
+
+		RaycastHit hitInfo;
+		if(collider.Raycast(ray, out hitInfo, Mathf.Infinity ) )
+		{
+			renderer.material.color = highlightColor;
+		}
+		else 
+		{
+			renderer.material.color = normalColor;
+		}
+
+	}
+	/*
+	// Update is called once per frame
+	void OnMouseOver(){
+		renderer.material.color = Color.blue;
+	}
+
+	void OnMouseExit()
+	{
+		renderer.material.color = Color.white;
+	}
+	*/
+}
