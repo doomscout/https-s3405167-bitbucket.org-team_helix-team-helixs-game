9bc2dbb0f6f0ffdd89a095988907def1c96ada7a
unknown <s3383494@student.rmit.edu.au>
2014-04-22 05:01:07 +1000

    Added code useful for debugging (TestTiming)

---
+++ Assets/Resources/Scripts/DebugScipts/TestTiming.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public delegate void TimeMethodInit();
+public delegate void TimeMethodStart();
+
+//This class is used for timing methods
+//Example of use: To test the BuildMesh() function, do TestTiming.timeMethod(new TimeMethodStart(BuildMesh));
+public static class TestTiming {
+	
+	public static void doNothingMethod() {
+		
+	}
+
+	public static void timeMethod(TimeMethodStart start) {
+		timeMethod(new TimeMethodInit(doNothingMethod), start);
+	}
+	
+	public static void timeMethod(TimeMethodInit init, TimeMethodStart start) {
+		Stopwatch s =  new Stopwatch();
+		init();
+		s.Start();
+		start();
+		s.Stop();
+		UnityEngine.Debug.LogError (s.ElapsedMilliseconds);
+	}
+
+}
