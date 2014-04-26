400dd8509dd5299027e80f4a6f0142aefc30cf1e
Isaac Chui <greycreeze2345@yahoo.com.au>
2014-04-26 14:30:38 +1000

    Merge branch 'master' of https://bitbucket.org/team_helix/team-helixs-game
    HelpMenu GUI Basic

---
+++ Assets/Resources/Scripts/GUIStuff/HelpGUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpGUI : MonoBehaviour {
+    public bool helpOn;
+	// Use this for initialization
+	void Start () {
+        helpOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI(){
+        if(GUI.Button(new Rect(0,100,20,20), "?")){
+            helpOn = !helpOn;
+        }
+        if (helpOn == true)
+        {
+            GUI.Box(new Rect(0, 120, 250, 100), "Help Content" +
+                "\nMovement - WASD keys" +
+                "\nClick on Magic then select tile to use");
+        }
+    }
+}
