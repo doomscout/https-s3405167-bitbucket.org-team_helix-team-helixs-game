74cdcbedd57d101186f75d49eabaf2b63ad5fd4f
Adrian Tang <s3292955@student.rmit.edu.au>
2014-04-27 14:46:48 +1000

    Skeleton inventory. Needs integration with UI.

--- Assets/Resources/Scripts/Player/Inventory.cs
+++ Assets/Resources/Scripts/Player/Inventory.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Inventory {
+	public List<Spell> spellInventory = new List<Spell>();
+
 	public Inventory() {
-		List<Spell> spellInventory = new List<Spell>();
+
 	}
 
 	public void addSpell() {
@@ -14,12 +16,45 @@ public class Inventory {
 	public void removeSpell() {
 
 	}
+
+	public void generateSpell() {
+		spellInventory.Add(new Spell());
+	}
 }
 
 public class Deck {
+	public List<Spell> spellDeck = new List<Spell>();
 
+	public Deck() {
+		for(int i = 0; i < 6; i++) {
+			spellDeck.Add(new Spell());
+		}
+	}
+
+	public void dropSpell(int element) {
+		spellDeck.RemoveAt(element);
+	}
+
+	public void addSpell() {
+		
+	}
+	
+	public void removeSpell() {
+		
+	}
 }
 
 public class DeckManager {
+	Inventory inv = new Inventory();
+	Deck deck = new Deck();
 
+	public void addToDeck() {
+		deck.addSpell();
+		inv.removeSpell();
+	}
+
+	public void removeFromDeck() {
+		inv.addSpell();
+		deck.removeSpell();
+	}
 }
