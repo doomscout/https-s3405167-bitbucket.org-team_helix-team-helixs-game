2deff672709d3d788ff52c2cafee5bdd8d929801
Isaac Chui <greycreeze2345@yahoo.com.au>
2014-04-29 22:37:10 +1000

    More details in the boxes such as colour shape and power

--- Assets/Resources/Scripts/GUIStuff/InventoryGUI.cs
+++ Assets/Resources/Scripts/GUIStuff/InventoryGUI.cs
@@ -5,12 +5,13 @@ public class InventoryGUI : MonoBehaviour {
  void OnGUI()
     {
 		if (GameTools.Player != null) {
-	        GUI.Box(new Rect(700,200,100,100),"Current Spell:" + "\n" + GameTools.Player.deckManager.peekTopSpell().Shape.spellShape 
-	                + "\n" + GameTools.Player.deckManager.peekTopSpell().SpellColour.ToString() );
-	        GUI.Box(new Rect(700,100,100,100),GameTools.Player.deckManager.getDeckSpell(1).Shape.spellShape 
-	                + "\n" + GameTools.Player.deckManager.getDeckSpell(1).SpellColour.ToString() );
-	        GUI.Box(new Rect(700,0,100,100),GameTools.Player.deckManager.getDeckSpell(2).Shape.spellShape 
-	                + "\n" + GameTools.Player.deckManager.getDeckSpell(2).SpellColour.ToString() );
+	        GUI.Box(new Rect(700,200,100,100),"Current Spell:" + "\nShape: " + GameTools.Player.deckManager.peekTopSpell().Shape.spellShape 
+                    + "\nColour: " + GameTools.Player.deckManager.peekTopSpell().SpellColour.ToString() + "\nPower: " + GameTools.Player.deckManager.getDeckSpell(0).Power );
+	        GUI.Box(new Rect(700,100,100,100),"\nShape: " + GameTools.Player.deckManager.getDeckSpell(1).Shape.spellShape 
+                    + "\nColour: " + GameTools.Player.deckManager.getDeckSpell(1).SpellColour.ToString() + "\nPower: " + GameTools.Player.deckManager.getDeckSpell(0).Power );
+	        GUI.Box(new Rect(700,0,100,100),"\nShape: " + GameTools.Player.deckManager.getDeckSpell(2).Shape.spellShape 
+                    + "\nColour: " + GameTools.Player.deckManager.getDeckSpell(2).SpellColour.ToString() + "\nPower: " + GameTools.Player.deckManager.getDeckSpell(1).Power);
+
 		}
 
     }
