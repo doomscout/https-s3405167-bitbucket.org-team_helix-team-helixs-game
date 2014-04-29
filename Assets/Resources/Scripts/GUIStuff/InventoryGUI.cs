3196ae1f1c3eee81157f2077dd86681c01cc7fb1
Isaac Chui <greycreeze2345@yahoo.com.au>
2014-04-29 20:45:53 +1000

    deck displaying now Inventory not done yet

---
+++ Assets/Resources/Scripts/GUIStuff/InventoryGUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryGUI : MonoBehaviour {
+ void OnGUI()
+    {
+        GUI.Box(new Rect(700,200,100,100),GameTools.Player.deckManager.peekTopSpell().Shape.spellShape 
+                + "\n" + GameTools.Player.deckManager.peekTopSpell().SpellColour.ToString() );
+        GUI.Box(new Rect(700,100,100,100),GameTools.Player.deckManager.getSpell(1).Shape.spellShape 
+                + "\n" + GameTools.Player.deckManager.getSpell(1).SpellColour.ToString() );
+        GUI.Box(new Rect(700,0,100,100),GameTools.Player.deckManager.getSpell(2).Shape.spellShape 
+                + "\n" + GameTools.Player.deckManager.getSpell(2).SpellColour.ToString() );
+
+    }
+}
