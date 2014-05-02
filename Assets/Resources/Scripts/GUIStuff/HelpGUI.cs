66a8bfdfb93af438e61b9f604f0535e18a6b10d1
Isaac Chui <greycreeze2345@yahoo.com.au>
2014-05-02 19:51:03 +1000

    Fixed some errors on Inventory GUI and made health bar much more noticeable

--- Assets/Resources/Scripts/GUIStuff/HelpGUI.cs
+++ Assets/Resources/Scripts/GUIStuff/HelpGUI.cs
@@ -22,14 +22,25 @@ public class HelpGUI : MonoBehaviour {
             GUI.Box(new Rect(0, 120, 250, 100), "Help Content" +
                 "\nMovement - WASD keys" +
                 "\nPress 1 and select Area- to Cast Spell"
-                 +"\n Space Bar - Skip Turn"   );
+                 +"\n Space Bar - Skip Turn" + "\n Press 'i' for Inventory"   );
         }
 
        
             //Debug.Log("GUIComplete");
 		if (GameTools.Player != null) {
-            GUI.Box(new Rect(100, 100, 100 + 10, 20), "" + GameTools.Player.stats.Health);
-            GUI.Box(new Rect(100, 100, (float)GameTools.Player.stats.Health/(float)GameTools.Player.stats.Max_Health * 100.0f + 10, 20), "");
+            //Health Bar
+            GUI.Box(new Rect(0, 60,5 * 100 + 10, 40), "Health" );
+            if (GameTools.Player.stats.Health < 0)
+            {
+                //Checks if health goes less than 0 and dsilpay 0
+                GUI.Box(new Rect(0, 80,5 * 100 + 10, 20), "0");
+            }
+            else
+            {
+            GUI.Box(new Rect(0, 80,5 * 100 + 10, 20), "" + GameTools.Player.stats.Health);
+            }
+            //Displays bar that decreases based on health
+            GUI.Box(new Rect(0, 80,  5*(float)GameTools.Player.stats.Health/(float)GameTools.Player.stats.Max_Health * 100.0f + 10, 20), "");
 		}
        
     }
